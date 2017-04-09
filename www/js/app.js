c630696bc735e0d9a65afa773cce4fe7b375ca13
EmOnTheWeb <emilie.dannenberg@gmail.com>
2017-04-09 15:14:49 +0100

    first

---
+++ www/js/app.js
@@ -0,0 +1,65 @@
+window.onload = function() {
+    document.addEventListener("deviceready", init, false);
+}
+function init() { //run everything in here only when device is ready
+
+	requestUri = 'http://localhost:8888/list-walks'; 
+
+	var xhr = new XMLHttpRequest();
+	    
+	xhr.open('GET',requestUri, true);
+	xhr.send(null);  
+
+	xhr.onreadystatechange = function() {
+	    if (xhr.readyState == XMLHttpRequest.DONE) { 
+	        var walks = xhr.responseText;
+	        readIntoSelect(walks);        
+	    }
+	}
+
+	function readIntoSelect(walks) {
+		walks= JSON.parse(walks);
+		var selectElem=document.querySelector(".choose-walk"); 
+		for(var i=0; i<walks.length; i++) {
+			var opt=document.createElement("option"); 
+			
+			opt.text=removeExtAndUnderscore(walks[i]); 
+
+			var value=walks[i].substring(0,walks[i].indexOf('.gpx'));
+			opt.value=walks[i].substring(0,walks[i].indexOf('.gpx')); 
+			selectElem.add(opt,null); 
+		}
+	} 
+	var startWalkBtn = document.querySelector(".start-walk");
+    startWalkBtn.addEventListener("click", getWalkDirections, false);
+
+}
+function removeExtAndUnderscore(filename) {
+
+	var removeExt=filename.substring(0,filename.indexOf('.gpx'));
+	var removeUnderscore=removeExt.replace(/_/g,' '); 
+	return removeUnderscore; 
+
+}
+function getWalkDirections() {
+
+    var select=document.querySelector(".choose-walk");
+    var selectedValue=select.value; 
+   	
+   	if(selectedValue !== '') {
+    requestUri = 'http://localhost:8888/get-directions/'+selectedValue; 
+
+		var xhr = new XMLHttpRequest();
+		    
+		xhr.open('GET',requestUri, true);
+		xhr.send(null);  
+
+		xhr.onreadystatechange = function() {
+		    if (xhr.readyState == XMLHttpRequest.DONE) { 
+		        var directions = xhr.responseText;   
+		        console.log(directions);  
+		    }
+		}
+	}
+}
+
