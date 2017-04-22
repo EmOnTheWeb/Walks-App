137eacf0b0b2ae2d2f5f48982b25c2904f51f074
EmOnTheWeb <emilie.dannenberg@gmail.com>
2017-04-22 17:20:24 +0100

    add walk to database

--- platforms/browser/www/js/app.js
+++ platforms/browser/www/js/app.js
@@ -19,6 +19,7 @@ function init() { //run everything in here only when device is ready
 
 	function readIntoSelect(walks) {
 		walks= JSON.parse(walks);
+		 
 		var selectElem=document.querySelector(".choose-walk"); 
 		for(var i=0; i<walks.length; i++) {
 			var opt=document.createElement("option"); 
@@ -29,10 +30,11 @@ function init() { //run everything in here only when device is ready
 			opt.value=walks[i].substring(0,walks[i].indexOf('.gpx')); 
 			selectElem.add(opt,null); 
 		}
-	} 
-	var startWalkBtn = document.querySelector(".start-walk");
-    startWalkBtn.addEventListener("click", getWalkDirections, false);
+	}
 
+	var startWalkBtn = document.querySelector(".start-walk");
+    
+    startWalkBtn.addEventListener("click", getWalkDirections, false);
 }
 
 function removeExtAndUnderscore(filename) {
@@ -48,30 +50,56 @@ function getWalkDirections() {
     var selectedValue=select.value; 
    	
    	if(selectedValue !== '') {
-    requestUri = 'http://localhost:8888/get-directions/'+selectedValue; 
+   		// if(walknotindatabase) {
+		    requestUri = 'http://localhost:8888/get-directions/'+selectedValue; 
 
-		var xhr = new XMLHttpRequest();
-		    
-		xhr.open('GET',requestUri, true);
-		xhr.send(null);  
+			var xhr = new XMLHttpRequest();
+			    
+			xhr.open('GET',requestUri, true);
+			xhr.send(null);  
 
-		var promiseObject = new Promise(function(resolve, reject){	
-			xhr.onreadystatechange = function() {
-		    	if (xhr.readyState == XMLHttpRequest.DONE) { 
-		    		if(xhr.status===200) {
-		        		var directions = xhr.responseText;   
-		        		resolve(directions);  
-		        	} else {
-		        		reject(xhr.status);
-		        	}
-		    	}
-			}
-		}); 
-		promiseObject.then(parseDirections, error); 
+			var promiseObject = new Promise(function(resolve, reject){	
+				xhr.onreadystatechange = function() {
+			    	if (xhr.readyState == XMLHttpRequest.DONE) { 
+			    		if(xhr.status===200) {
+			        		var directions = xhr.responseText;   
+			        		resolve(directions);  
+			        	} else {
+			        		reject(xhr.status);
+			        	}
+			    	}
+				}
+			}); 
+			promiseObject.then(addWalkToDatabase, error); 
+		// }
 	}
 }
-function parseDirections(directions) {
+function addWalkToDatabase(directions) {
+	
+	directions = JSON.parse(directions); 
+	var route = directions.routes[0]; 
+	var legs = route.legs; //a leg is a route between two waypoints 	
 	console.log(directions); 
+	// 	var route = directionObject['routes'][0]; 
+	// var steps = route['steps'];
+
+	// var stepsRelevantData = []; 
+	// for (var i= 0; i< steps.length; i++) {
+
+	// 	var currentStep=steps[i]; 
+
+	// 	var direction=currentStep['direction']; 
+	// 	var distance=currentStep['distance']; 
+	// 	var instruction=currentStep['maneuver']['instruction']; 
+	// 	var type=currentStep['maneuver']['type']; 
+	// 	var coordinates=currentStep['maneuver']['location']['coordinates']; //coordinates are in [longitude, latitude] for google maps lat long goes the other way!
+
+	// 	stepsRelevantData.push({coordinates:coordinates, distance:distance, direction:direction, type:type, instruction: instruction})
+	// 	console.log(coordinates); 
+	// 	//testing purposes
+	// 	document.getElementById('key-coordinates').innerHTML+="Latitude: "+coordinates[1]+", Longitude "+coordinates[0]+ "</br>";  
+	// }
+	// return stepsRelevantData; 
 }
 function error(status) {
 	console.log('failed with status code' + status); 
