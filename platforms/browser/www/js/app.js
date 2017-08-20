b5b17b80d256f7bc89036c1b1fa809e3867d477b
emilie-purr <emiliedannenberg@purrdigital.com>
2017-08-20 11:00:53 +0100

    output as text to test

--- platforms/browser/www/js/app.js
+++ platforms/browser/www/js/app.js
@@ -8,6 +8,7 @@ storage = Lawnchair({name: 'walk-information'}, function(e) {
 
 storage.keys('keys.forEach(console.log)'); 
 
+
 function init() { //run everything in here only when device is ready
 
 	var requestUri = 'http://api-walks.emiliedannenberg.co.uk/list-walks'; 
@@ -279,7 +280,7 @@ function getLandmarkDescriptions(walkName) {
 	    }
 	}
 }
-
+var showMsgDiv = document.querySelector(".waypoint-text");
 function startTracking(walkData, map) {
 	// var time = 0; 
 	console.log(walkData); 
@@ -321,31 +322,35 @@ function startTracking(walkData, map) {
         			//compare geoposition to step position 
         			if(isClose(currentLat, currentLng, stepLat, stepLng)) {
         				
-        				var msg = new SpeechSynthesisUtterance();//ur gunna say something!!
+        				// var msg = new SpeechSynthesisUtterance();//ur gunna say something!!
 
         				//if step type is arrive you're at a waypoint, get waypoint info	
         				if(currentStep.type==="arrive" && notAtEnd(stepLocation, coordinateData.end)) {
         					//get waypoint info. 
         					console.log('you are at a waypoint');
         					//get leg, get corresponding waypoint info index
-        					var waypointDescription = getWaypointDescription(i,walkData.landmarkDescriptions); 
-        					msg.text = waypointDescription; 
+        					var waypointDescription = getWaypointDescription(i,walkData.landmarkDescriptions);    
+ 			 				var msg = waypointDescription; 
+        					// msg.text = waypointDescription; 
         				} 
         				else if(currentStep.type==="arrive" && !notAtEnd(stepLocation, coordinateData.end)) { // you're at the end
-        					msg.text = 'walk finished'; 
+        					// msg.text = 'walk finished'; 
+        					var msg = 'walk finished'; 
         				}
         				else if(stepLocation.join() === coordinateData.beginning) {
-        					msg.text = 'beginning of walk'; 	
+        					// msg.text = 'beginning of walk'; 	
+        					var msg = 'beginning of walk'; 
         				}	
         				else {	 // get instruction 
 
         					var instruction = currentStep.instruction; 
         					//read this out 
-        					msg.text = instruction; 
+        					// msg.text = instruction; 
+        					var msg = instruction; 
         				}
-
+        				showMsgDiv.innerHTML += '<p>' + msg + '</p>'; 
         				//say your thang
-        				window.speechSynthesis.speak(msg);
+        				// window.speechSynthesis.speak(msg);
         				//now break out of everything
         				j = legSteps.length; 
         				i = journeyLegs.length; 
@@ -366,9 +371,10 @@ function startTracking(walkData, map) {
 }
 
 function getWaypointDescription(legIndex, descriptions) {
+	debugger; 
 	var infoIndex = legIndex; 
-	var descriptions = descriptions.replace(/(?:\r)/g, '<br />');
-	var split = descriptions.split('<br />'); 
+	// var descriptions = descriptions.replace(/(?:\r)/g, '<br />');
+	var split = descriptions.split(','); 
 	
 	return split[infoIndex].trim(); 
 }
